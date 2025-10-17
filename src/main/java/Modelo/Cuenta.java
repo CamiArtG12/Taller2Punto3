7497a6ae70460df4558cbd245e0ad02cd215f649
CamiArtG12 <kmigamer16@gmail.com>
2025-10-17 00:31:55 -0500

    hicimos las clases ArrayListCuenta, Banco, Cuenta, CuentaAhorros, CuentaCorriente y la clase main pricipal que es Taller2Punto3 Se hizo una nueva clase llamada fecha

--- src/main/java/Modelo/Cuenta.java
+++ src/main/java/Modelo/Cuenta.java
@@ -9,5 +9,53 @@ package Modelo;
  * @author CamiLaNekoUwU_Gamer
  */
 public class Cuenta {
+    private int numero;
+    private String nombreDueno;
+    private Fecha fechaApertura;
+    private double saldo;
+
+    public Cuenta(int numero, String nombreDueno, Fecha fechaApertura, double saldo) {
+        this.numero = numero;
+        this.nombreDueno = nombreDueno;
+        this.fechaApertura = fechaApertura;
+        this.saldo = 0.0;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNombreDueno() {
+        return nombreDueno;
+    }
+
+    public void setNombreDueno(String nombreDueno) {
+        this.nombreDueno = nombreDueno;
+    }
+
+    public Fecha getFechaApertura() {
+        return fechaApertura;
+    }
+
+    public void setFechaApertura(Fecha fechaApertura) {
+        this.fechaApertura = fechaApertura;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    public void consignar(double monto) {
+        if (monto > 0) {
+            this.saldo += monto;
+        }
+    }
     
 }
