f67da58178d32e86fe84558993d2a520d31d2738
CamiArtG12 <kmigamer16@gmail.com>
2025-10-17 00:14:12 -0500

    hicimos las clases ArrayListCuenta, Banco, Cuenta, CuentaAhorros, CuentaCorriente y la clase main pricipal que es Taller2Punto3

---
+++ src/main/java/com/mycompany/taller2punto3/Taller2Punto3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller2punto3;
+
+/**
+ *
+ * @author CamiLaNekoUwU_Gamer
+ */
+public class Taller2Punto3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
